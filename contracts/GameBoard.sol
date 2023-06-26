d1b967ef52e42c59f01f42205d9280786966d921
francocco99 <fracier@hotmail.it>
2023-06-26 21:52:09 +0200

    aggiunto

--- contracts/GameBoard.sol
+++ contracts/GameBoard.sol
@@ -1,7 +1,52 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+contract BoardGame {
+  
+   // we take the root beacuse anyone can check if we change the initial choiche
+   // at the end of the game anyone can check the board correspond the board have previous committed
 
-contract GameBoard {
-  constructor() public {
+  // Players  Merkle tree root
+  mapping(address => bytes32) plyrsMerkleRt;
+  enum phase {WaitP,first, Second, Therd}
+  address public firstPlayer;
+  address public SecondPlayer;
+  phase public currphase;
+
+
+    //memorizzo il merkel tree dell'utente
+  function storeRoot(bytes32 Rootm) public
+  {
+    plyrsMerkleRt[msg.sender]=Rootm;
+  }
+
+   // controllo in che fase mi trovo
+  function check_phase() public
+  {
+  
+  }
+  
+// controllo che l'avversario non stia barando
+  function Proof(
+        bool Ship,
+        uint256 salt,
+        uint8 ix,
+        bytes32[] memory proof,
+        bytes32 root
+    ) internal pure returns (bool) {
+      bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(salt,Ship, ix)))); 
+
+        return MerkleProof.verify(proof, root, leaf);
+    }
+
+  // controllo che la posizione sia corretta
+  function  shot(uint8 pos) internal
+  {
+
+  }
+
+  constructor(address creator) 
+  {
+    
   }
 }
--- contracts/GameRule.sol
+++ contracts/GameRule.sol
@@ -1,7 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
+  contract GameRule {
+    uint IncremId;
+    //BattleshipGame[] private joinableGames;
+    event GameStart(address indexed _from, address indexed _to);
+    event GameCreated();
+    
+    struct game{
+        address creator;
+        uint id;
+        phase p;
+      
+    }
+    game [] public Listgames;
+    
+    function createGame() public 
+    {
+        IncremId=IncremId+1;
+        Listgames.push(game(msg.sender,IncremId,phase.WaitP));
+        emit GameCreated();
+        return;
+    }
+    constructor() public
+    {
 
-contract GameRule {
-  constructor() public {
-  }
+    }
+    
+    
 }
+
